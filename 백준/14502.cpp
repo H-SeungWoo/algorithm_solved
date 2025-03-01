70f139097dd0b1e96a871ae44e434cb5d2b04b11
한승우 <tmddn_00@naver.com>
2025-03-02 00:37:03 +0900

    수정

--- 백준/14502.cpp
+++ 백준/14502.cpp
@@ -4,34 +4,43 @@ using namespace std;
 
 int dy[4] = {-1, 0 , 1, 0};
 int dx[4] = {0, 1, 0, -1};
-int n, m, a[10][10], visited[10][10], ret;
+int n, m, a[10][10], visited[10][10], ret, ny, nx;
 vector<pair<int, int>> virusList, spaceList;
 
-void dfs(int y, int x){
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || a[ny][nx] == 1) continue;
-        visited[ny][nx] = 1;
-        dfs(ny, nx);
+void dfs(int y, int x)
+{
+    visited[y][x] = 1;
+    for(int i =0; i<4; i++)
+    {
+        ny = y + dy[i];
+        nx = x + dx[i];
+
+        if(ny<0|| nx<0 || ny>=n || nx >= m) continue;
+        if(a[ny][nx] == 0 && !visited[ny][nx])
+        {
+            dfs(ny, nx);
+        }
     }
-    return;
 }
 
-int solve(){
-    fill(&visited[0][0], &visited[0][0] + 10 * 10, 0); 
-    for(pair<int, int> b : virusList){
-        visited[b.first][b.second] = 1;
-        dfs(b.first, b.second);
-    } 
+int solve()
+{
+    fill(&visited[0][0], &visited[0][0]+10*10, 0);
+    for(pair<int, int> v: virusList)
+    {
+        dfs(v.first, v.second);
+    }
 
     int cnt = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] == 0 && !visited[i][j])cnt++;
+    for (int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(a[i][j]==0 && !visited[i][j]) cnt++;
         }
-    } 
-    return cnt;  
+    }
+
+    return cnt;
 }
 
 int main()
@@ -54,9 +63,9 @@ int main()
         {
             for(int k=0; k<j; k++)
             {
-                a[spaceList[i].first][spaceList[i].second] == 1;
-                a[spaceList[j].first][spaceList[j].second] == 1;
-                a[spaceList[k].first][spaceList[k].second] == 1;
+                a[spaceList[i].first][spaceList[i].second] = 1;
+                a[spaceList[j].first][spaceList[j].second] = 1;
+                a[spaceList[k].first][spaceList[k].second] = 1;
                 ret = max(ret, solve());
                 a[spaceList[i].first][spaceList[i].second] = 0;
                 a[spaceList[j].first][spaceList[j].second] = 0;
@@ -65,5 +74,7 @@ int main()
 
         }
     }
+
+    cout << ret << "\n";
     return 0;
 }
