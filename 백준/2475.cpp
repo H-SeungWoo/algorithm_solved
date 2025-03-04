6fd4a49e6be82250b8af101eae9b6e4bcc6d0d47
한승우 <tmddn_00@naver.com>
2025-03-05 00:25:56 +0900

    사칙연산

---
+++ 백준/2475.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, sum = 0;
+
+int main()
+{
+    for(int i=0; i<5; i++)
+    {
+        cin>>n;
+        sum += n*n;
+    }
+
+    cout << sum%10 << '\n';
+}
