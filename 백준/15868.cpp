2e54121fc11c921b0de12fe5cb0b599f9e6aaac3
한승우 <tmddn_00@naver.com>
2025-03-05 00:20:30 +0900

    완전탐색

--- 백준/15868.cpp
+++ 백준/15868.cpp
@@ -5,43 +5,51 @@ using namespace std;
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {1, 0 ,-1 ,0};
 
-int n, m, ny, nx, a[54][54], cDist = 0;
-vector<pair<int, int>> homeList, storeList;
+int n, m, ny, nx, a[54][54], cDist = 25000000;
+vector<pair<int, int>> homeList, storeList, testList;
 
 // 1. 모든 치킨집이 있는 경우와 없는 경우에서 (2^m의 경우의 수 )
 // 2. 한 집과 다른 치킨집들 사이에 최단거리를 구한다.
 
-// 치킨가게 배치하는 함수
-int GetcDist()
+int GethDist(int x1, int x2, int y1, int y2)
 {
-    for(auto startPos: homeList)
-    {
+    return abs(x2-x1) + abs(y2-y1);
+}
 
+// 치킨가게 배치하는 함수
+int GetcDist(vector<pair<int, int>> v)
+{
+    int sum = 0;
+    for(auto home: homeList)
+    {
+        int dist = 5010;
+        for(auto store: v)
+        {
+            dist = min(dist, GethDist(home.first,store.first, home.second, store.second));
+        }
+
+        sum += dist;
     }
 
+    return sum;
 }
 
 // m개의 치킨집을 고르는 함수
 void solve(int index, vector<pair<int, int>> v)
 {
-    if(index == m)
+    if(v.size() == m)
     {
-        for(auto store: v)
-        {
-            a[store.first][store.second] = 2;
-        }
-        cDist = min(cDist, GetcDist());
-        for(auto store: v)
-        {
-            a[store.first][store.second] = 0;
-        }
+        cDist = min(cDist, GetcDist(v));
         return;
     }
+
+    if(index == storeList.size()) return;
 
     v.push_back(storeList[index]);
     solve(index +1, v);
     v.pop_back();
     solve(index+1, v);
+    
 }
 
 
@@ -54,17 +62,15 @@ int main()
         {
             cin >> a[i][j];
             if(a[i][j] == 1) homeList.push_back({i, j});
-            if(a[i][j] == 2) 
-            {
-                a[i][j] = 0;
-                storeList.push_back({i, j});
-            }
+            if(a[i][j] == 2) storeList.push_back({i, j});
         }
-
-        //combi(-1);
     }
 
+    // for(auto home: homeList) cout << "(" <<home.second<<","<<home.first<<") ";
+    // cout << '\n';
+    // for(auto store: storeList) cout << "(" <<store.second<<","<<store.first<<") ";
+    // cout << '\n';
 
-
-
+    solve(0, testList);
+    cout << cDist << '\n';
 }
