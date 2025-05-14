b62c2faca04002dcbc1a5d73f9d88f8351c2c14e
한승우 <tmddn_00@naver.com>
2025-05-14 23:53:39 +0900

    맵

---
+++ 백준/10816.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+map<int, int> cards;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int temp;
+        cin >> temp;
+        cards[temp]++;
+    }
+
+    cin>>m;
+    for(int i=0; i<m; i++)
+    {
+        int temp;
+        cin>> temp;
+        cout << cards[temp] << ' ';
+    }
+
+    cout << '\n';
+}
