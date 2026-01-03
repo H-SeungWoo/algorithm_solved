c53cefa8a968b8909fcb60a281fed0b267d4ed07
한승우 <107747566+H-SeungWoo@users.noreply.github.com>
2026-01-03 14:50:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1560 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, temp, cnt[10001];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>temp;
+        cnt[temp]++;
+    }
+
+    for(int i=1; i<=10000; i++){
+        int a = cnt[i];
+        while(a--){
+            cout<<i<<'\n';
+        }
+    }
+
+    return 0;
+}
