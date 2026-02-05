a954f104a7cd16348cb10dd201b529489e1fedba
한승우 <107747566+H-SeungWoo@users.noreply.github.com>
2026-02-05 10:01:46 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a, b, c, d, e;
+
+int main(){
+    for(int i=0; i<2; i++){
+        cin>>a>>b>>c>>d>>e;
+        cout<<a*6+b*3+c*2+d+e*2<<'\n';
+    }
+
+    return 0;
+}
